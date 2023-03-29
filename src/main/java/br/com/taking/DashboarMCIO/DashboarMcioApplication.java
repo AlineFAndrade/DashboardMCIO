9ecb55667b6eb1e26be4ed7359a53bf455683dcc
Aline Andrade <marson.aline@gmail.com>
2023-03-29 18:07:18 -0300

    first commit

---
+++ src/main/java/br/com/taking/DashboarMCIO/DashboarMcioApplication.java
@@ -0,0 +1,13 @@
+package br.com.taking.DashboarMCIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboarMcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboarMcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/taking/DashboarMCIO/DashboarMcioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taking.DashboarMCIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboarMcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
