de07f6b81b74ad13c66825254da0c9659936d535
Aline Andrade <marson.aline@gmail.com>
2023-04-18 19:35:16 -0300

    testando commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-// routes
+
 import Router from './routes';
 // theme
 import ThemeProvider from './theme';
--- Frontend/src/_mock/user.js
+++ Frontend/src/_mock/user.js
@@ -1,7 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { sample } from 'lodash';
 
-// ----------------------------------------------------------------------
 
 const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
